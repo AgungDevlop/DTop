99dadfe6ee23233234278d5f108267c89dddd97b
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2025-02-05 23:51:15 +0800

    Update PlayVideo.tsx

--- src/pages/PlayVideo.tsx
+++ src/pages/PlayVideo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { FaCopy, FaVideo, FaPlay, FaSearch, FaDownload } from 'react-icons/fa';
 
